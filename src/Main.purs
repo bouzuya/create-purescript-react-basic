d0f107fb6f2db206d2cb9fe570014fe371334d7b
bouzuya <m@bouzuya.net>
2018-11-27 22:01:49 +0900

    add license template

--- src/Main.purs
+++ src/Main.purs
@@ -1,8 +1,18 @@
 module Main (main) where
 
+import Data.Either (either)
 import Effect (Effect)
-import Effect.Console (log)
-import Prelude (Unit)
+import Effect.Aff (runAff_)
+import Effect.Exception (throwException)
+import Node.Encoding as Encoding
+import Node.FS.Aff as Fs
+import Prelude (Unit, bind, pure, unit)
 
 main :: Effect Unit
-main = log "Hello"
+main = do
+  runAff_ (either (throwException) pure) do
+    license <- Fs.readTextFile Encoding.UTF8 "templates/LICENSE"
+    _ <- Fs.writeTextFile Encoding.UTF8 "LICENSE" license
+    readme <- Fs.readTextFile Encoding.UTF8 "templates/README.md"
+    _ <- Fs.appendTextFile Encoding.UTF8 "README.md" readme
+    pure unit
---
+++ templates/README.md
@@ -0,0 +1,11 @@
+## License
+
+[MIT](LICENSE)
+
+## Author
+
+[bouzuya][user] &lt;[m@bouzuya.net][email]&gt; ([https://bouzuya.net/][url])
+
+[user]: https://github.com/bouzuya
+[email]: mailto:m@bouzuya.net
+[url]: https://bouzuya.net/

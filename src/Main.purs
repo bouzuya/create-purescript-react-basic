7e0ee7cdd1cc17f0f3ffa1d3290f91a92b516cd7
bouzuya <m@bouzuya.net>
2018-11-29 18:50:52 +0900

    fix template dir path

--- src/Main.purs
+++ src/Main.purs
@@ -6,13 +6,16 @@ import Effect.Aff (Aff, runAff_)
 import Effect.Exception (throwException)
 import Node.Encoding as Encoding
 import Node.FS.Aff as Fs
+import Node.Globals (__dirname)
+import Node.Path as Path
 import Prelude (Unit, bind, pure, unit)
 
 addLicenseAndUpdateReadme :: Aff Unit
 addLicenseAndUpdateReadme = do
-  license <- Fs.readTextFile Encoding.UTF8 "templates/LICENSE"
+  dir <- pure (Path.concat [__dirname, "templates"])
+  license <- Fs.readTextFile Encoding.UTF8 (Path.concat [dir, "LICENSE"])
   _ <- Fs.writeTextFile Encoding.UTF8 "LICENSE" license
-  readme <- Fs.readTextFile Encoding.UTF8 "templates/README.md"
+  readme <- Fs.readTextFile Encoding.UTF8 (Path.concat [dir, "README.md"])
   _ <- Fs.appendTextFile Encoding.UTF8 "README.md" readme
   pure unit
 

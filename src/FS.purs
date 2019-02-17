25813d83e7fa005ecd453e26650a144ec5f598c9
bouzuya <m@bouzuya.net>
2019-02-18 08:47:36 +0900

    use >>=

--- src/FS.purs
+++ src/FS.purs
@@ -10,7 +10,7 @@ import Effect.Aff (Aff)
 import Node.Encoding as Encoding
 import Node.FS.Aff as Fs
 import Node.Path as Path
-import Prelude (Unit, bind, discard, pure, unit)
+import Prelude (Unit, bind, discard, flip, unless, (>>=))
 
 appendTextFile :: String -> String -> Aff Unit
 appendTextFile src dst = do
@@ -19,14 +19,10 @@ appendTextFile src dst = do
   Fs.appendTextFile Encoding.UTF8 dst d
 
 copyTextFile :: String -> String -> Aff Unit
-copyTextFile src dst = do
-  d <- readTextFile src
-  writeTextFile dst d
+copyTextFile src dst = readTextFile src >>= writeTextFile dst
 
 makeDirectory :: String -> Aff Unit
-makeDirectory dir = do
-  dirExists <- Fs.exists dir
-  if dirExists then pure unit else Fs.mkdir dir
+makeDirectory dir = Fs.exists dir >>= flip unless (Fs.mkdir dir)
 
 readTextFile :: String -> Aff String
 readTextFile = Fs.readTextFile Encoding.UTF8
